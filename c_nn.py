54ad94006581c48f7885906d8a70ae0ab000c649
RheeJ <JoonyongRhee2016@u.northwestern.edu>
2017-01-05 11:24:10 -0600

    indentation fix

--- c_nn.py
+++ c_nn.py
@@ -38,26 +38,26 @@ def model(data_x):
 	fc5_b = tf.Variable(tf.constant(.1, shape=[10]))
 
 	c1 = tf.nn.conv2d(data_x, c1_w, strides=[1,1,1,1], padding='SAME') + c1_b
-    c1 = tf.nn.relu(c1)
-    c1 = tf.nn.max_pool(c1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+	c1 = tf.nn.relu(c1)
+	c1 = tf.nn.max_pool(c1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 
-    #print conv_l1.shape()
+	#print conv_l1.shape()
 
-    c2 = tf.nn.conv2d(c1, c2_w, strides=[1,1,1,1], padding='SAME') + c2_b
-    c2 = tf.nn.relu(c2)
-    c2 = tf.nn.max_pool(c2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+	c2 = tf.nn.conv2d(c1, c2_w, strides=[1,1,1,1], padding='SAME') + c2_b
+	c2 = tf.nn.relu(c2)
+	c2 = tf.nn.max_pool(c2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 
-    c3 = tf.nn.conv2d(c2, c3_w, strides=[1,1,1,1], padding='SAME') + c3_b
-    c3 = tf.nn.relu(c3)
-    c3 = tf.nn.max_pool(c3, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+	c3 = tf.nn.conv2d(c2, c3_w, strides=[1,1,1,1], padding='SAME') + c3_b
+	c3 = tf.nn.relu(c3)
+	c3 = tf.nn.max_pool(c3, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 
-    fc4 = tf.reshape(c3, [-1, 128 * 16 * 16])
-    fc4 = tf.matmul(fc4, fc4_w) + fc4_b
-    fc4 = tf.nn.relu(fc4)
+	fc4 = tf.reshape(c3, [-1, 128 * 16 * 16])
+	fc4 = tf.matmul(fc4, fc4_w) + fc4_b
+	fc4 = tf.nn.relu(fc4)
 
-    fc5 = tf.matmul(fc4, fc5_w) + fc5_b
+	fc5 = tf.matmul(fc4, fc5_w) + fc5_b
 
-    return fc5
+	return fc5
 def test(sess, test_x, test_y):
 	return sess.run([tf.argmax(test_y, 1), tf.argmax(logits,1)], feed_dict={X: test_x, Y: test_y})
 def train(learn_rate):
