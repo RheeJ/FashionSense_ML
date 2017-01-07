31f680705753ffc0e1971e05ee85b9f4d2eac28e
jsong1996 <jimmysong2018@u.northwestern.edu>
2017-01-07 15:35:21 -0600

    added error testing

--- d_tree.py
+++ d_tree.py
@@ -3,6 +3,9 @@
 
 import sys
 import pickle
+import sklearn
+import numpy as np
+import scipy
 from sklearn import tree
 
 InputFileName = str(sys.argv[1])
@@ -10,23 +13,44 @@ Query = str(sys.argv[2])
 
 def preprocess(filename):
     #TODO: Preprocess input file once images have been scraped.
-    pass
+    #Return training set, training labels, testing set, and testing labels as numpy array.
+    training_set = np.zeros()
+    training_labels = np.zeros()
+    testing_set = np.zeros()
+    testing_labels = np.zeros()
+
+
+    return training_set, training_labels, testing_set, testing_labels
 
 def save_classifier(classifier, training_set, training_labels):
-    pickle.dump(classifier, open('classifier_1.p', 'w'))
-    pickle.dump(training_set, open('training_set_1.p', 'w'))
-    pickle.dump(training_labels, open('training_labels.p_1', 'w'))
+    #Saves Classifier for faster runtime in future uses.
+    pickle.dump(classifier, open('classifier.p', 'w'))
+    pickle.dump(training_set, open('training_set.p', 'w'))
+    pickle.dump(training_labels, open('training_labels.p', 'w'))
 
 def main(filename, query):
-    training_set, training_labels = preprocess(filename)
+    training_set, training_labels, testing_set, testing_labels = preprocess(filename)
 
     classifier = tree.DecisionTreeClassifier()
     classifier = classifier.fit(training_set, training_labels)
     save_classifier(classifier, training_set, training_labels)
     classifier = pickle.load(open('classifier_1.p'))
-    result = classifier.predict(query)
 
-    print result
+
+    #Simple Error Test for Classifier
+    #TODO: Cross-Validation Testing for Error Rate
+    predicted_labels = classifier.predict(testing_set)
+    errorCount = 0
+    for i in range(len(testing_set)):
+        if testing_labels[i] != predicted_labels[i]:
+            errorCount += 1
+    errorRate = float(errorCount) / len(testing_set)
+    print "Error Rate of Decision_Tree Classifier: " + str(errorRate)
+
+    #Given new Query, Return classification of Query
+    result = classifier.predict(query)
+    print "Query Classified as: " + str(result)
     return result
 
 main(InputFileName)
+
