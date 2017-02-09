cc1c4c430b8a740f868e38a6cb1c329756384da9
rawitscher <lindsayrawitscher2017@u.northwestern.edu>
2017-02-08 22:35:23 -0600

    making it take in N models

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 import json
 from flask import request
 from flask import Flask
-import os
+import sys, os
 from binary_classifier_CNN import bcCNN
+
+def usage_message():
+    print "This script connects bCNNs to the endpoint host (i think)"
+    print "Usage: python app.py [-m=<module names>]"
+    exit()
+
+log('running')
+
+args = sys.argv[1:]
+
+if len(args) < 1:
+    usage_message()
+
+trained_models = args[0]
 
 app = Flask(__name__)
 
@@ -11,19 +25,30 @@ def index():
 
     filepath = request.get_json()["file_path"]
 
-    model_path = "./classifiers/formal/"
-    formal_net = bcCNN.Net(model_path)
-    classification = formal_net.classify(filepath)
+    # model_path = "./classifiers/formal/"
+    # changed from above to accomodate multiple
+    model_paths = []
+    for model in trained_models:
+        model_paths.append("./classifiers/"+model+"/")
+    print model_paths
 
-    if classification == 0:
-        classification = "formal"
-    else:
-        classification = "not formal"
+    # formal_net = bcCNN.Net(model_path)
+    # changed from above to accomodate multiple
+    classifications = []
+    for path in model_paths:
+        model_net = bcCNN.Net(path)
+        classification = model_net.classify(filepath)
+        if classification == 1:
+            classifications.append(path[14:-1])
+        else:
+            classifications.append("not" + path[14:-1])
+        print classifications
+    classifications_dict = dict(zip(trained_models,classifications))
 
-    dictionary = { "classification" : classification }
-    result = json.dumps(dictionary)
+    #dictionary = { "classification" : classification }
+    result = json.dumps(classifications_dict)
 
     return result
 
-# #app.run(debug=True)
+app.run(debug=True)
 # app.run(host="0.0.0.0")
