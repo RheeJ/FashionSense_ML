69b3e5b7926ff693ca6ffe2ad268eeb7bf12b719
rawitscher <lindsayrawitscher2017@u.northwestern.edu>
2017-02-14 11:10:34 -0600

    stay tuned

--- boto.py
+++ boto.py
@@ -4,4 +4,14 @@ s3 = boto3.resource('s3')
 
 # Print out bucket names
 for bucket in s3.buckets.all():
-    print(bucket.name)
+    print "Now accessing: "(bucket.name)
+
+
+bucket = s3.Bucket('imagedataset')
+exists = True
+try:
+    s3.meta.client.head_bucket(Bucket='imagedataset')
+except botocore.exceptions.ClientError as e:
+    error_code = int(e.response['Error']['Code'])
+    if error_code == 404:
+        exists = False
