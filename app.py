f56624e4e80942c74a4e6314df2771a8a70218d8
sgp715 <7SebastianPerez@gmail.com>
2017-02-15 19:38:18 -0600

    taking image directly

--- app.py
+++ app.py
@@ -1,25 +1,39 @@
 import json
+import tempfile
 from flask import request
 from flask import Flask
+from flask import abort
 import sys, os
 from binary_classifier_CNN import bcCNN
 from utils import get_classifications
 
-
 app = Flask(__name__)
-
 @app.route('/', methods=['POST'])
 
 def index():
-    filepath = request.get_json()["file_path"]
-    classifications_dict = get_classifications(image)
+
+    # write binary data to temp file
+    data = request.get_data()
+    if data == None:
+        abort(400)
+
+    temp = tempfile.TemporaryFile()
+    temp.write(data)
+    temp.seek(0)
+
+    # get the classifications from net
+    classifications_dict = get_classifications(temp)
+
+    # close temp file
+    temp.close()
+
     result = json.dumps(classifications_dict)
     return result
 
 if __name__ == "__main__":
 
-   debug = os.environ.get('DEBUG')
-   if debug:
-       app.run(debug=True)
-   else:
-       app.run(host="0.0.0.0")
+    debug = os.environ.get('DEBUG')
+    if debug:
+        app.run(debug=True)
+    else:
+        app.run(host="0.0.0.0")
