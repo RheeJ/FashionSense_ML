9649c3ab8834cf0a2a1fdc2952cffd128dd81bb1
rawitscher <lindsayrawitscher2017@u.northwestern.edu>
2017-04-02 12:39:43 -0500

    stuff

--- initialize.py
+++ initialize.py
@@ -3,7 +3,35 @@ import os
 import utils
 import boto3
 import shutil
+'''
+# Shortening the s3 command, nothing special
+s3 = boto3.resource('s3')
+client = boto3.client('s3')
+# Pull images from AWS bucket
+if not(os.path.isdir("./images")):
+    os.makedirs("./images")
+bucket = s3.Bucket("imagedataset")
 
+folders = []
+paginator = client.get_paginator('list_objects')
+for result in paginator.paginate(Bucket="imagedataset", Delimiter='/'):
+    for prefix in result.get('CommonPrefixes'):
+        tmp = str(prefix['Prefix'])
+        folders.append(tmp)
+        if not os.path.exists(tmp):
+            os.makedirs(tmp)
+
+
+for aws_object in bucket.objects.all():
+    aws_key = aws_object.key
+    print aws_key
+    print folders
+    if aws_key in folders:
+        continue     
+    dest = "images/"+"-".join(set(aws_key.split("/")))
+    s3.meta.client.download_file(bucket.name, aws_object.key, dest)
+
+'''
 
 conn = sqlite3.connect('database/database.db')
 c = conn.cursor()
@@ -27,53 +55,24 @@ except:
 
 classifiers = utils.get_classifiers()
 
-# Shortening the s3 command, nothing special
-s3 = boto3.resource('s3')
-client = boto3.client('s3')
-# Pull images from AWS bucket
-if not(os.path.isdir("./images")):
-    os.makedirs("./images")
-bucket = s3.Bucket("imagedataset")
-#folders = bucket.list("","/")
-#names = []
-#for i in range(len(folders)):
-#    names[i] = folders[i].name
-folders = []
-paginator = client.get_paginator('list_objects')
-for result in paginator.paginate(Bucket="imagedataset", Delimiter='/'):
-    for prefix in result.get('CommonPrefixes'):
-        folders.append((prefix.get('Prefix')))
-
-print folders
-for folder in folders:
-    try:
-        s3.Object(bucket.name, target).get()
-    except ClientError:
-        print "Target "+target+" not found"
-        os._exit(3)
-    for Object in bucket.objects.all():
-        # Can be changed to just positive images that we want to categorize
-        s3.meta.client.download_file(bucket.name, Object, "./images")
-
-'''
-for image_path in list(os.walk("./images"))[1:]:
-
+walk = list(os.walk("./images"))
+files = walk[0][2]
+print files
+for file in files:
+    image_path = walk[0][0]+file
     insert_string = "INSERT INTO images VALUES ( " + image_path + ','
-    classifications = get_classifications(classifiers, image_path)
-#images_dir = "./images"
-#for image in list(os.walk(images_dir))[0][2]:
-#    image_path = images_dir + '/' + image
-#   insert_string = "INSERT INTO images VALUES ( '" + image_path + "',"
-#    classifications = utils.get_classifications(classifiers, image_path)
+    classifications = utils.get_classifications(classifiers, image_path)
     for cat in categories:
         insert_string += (' ' + str(classifications[cat])  + ',')
-
+        print "hi"
     insert_string = insert_string[:-1]
+    print "hello"
+    print insert_string
     insert_string += ' )'
     c.execute(insert_string)
 
-shutil.rmtree("./images")
+#shutil.rmtree("./images")
 
 conn.commit()
 conn.close()
-'''
+
